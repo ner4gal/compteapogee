2d73946dfa0ec8174f0f57b252082ea26edc896e
ner4gal <karimelalkaoui@gmail.com>
2025-04-10 10:56:05 +0000

    after image login editing li

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -116,7 +116,7 @@
                             <li class="nav-main-item">
                                     <a class="nav-main-link" href="{{ route('inscription-annee-anterieure') }} ">
                                         <i class="fa fa-file-word fa-2x text-muted"></i>
-                                        <span class="nav-main-link-name ms-3">Inscription Administrative</span>
+                                        <span class="nav-main-link-name ms-3">Inscription Administrative ( Licence / Master )</span>
                                     </a>
                                 </li>
                                 <li class="nav-main-item">
@@ -128,19 +128,19 @@
                                 <li class="nav-main-item">
                                     <a class="nav-main-link" href="{{ route('insertion.resultat.etudiant') }}">
                                         <i class="fa fa-file-word fa-2x text-muted"></i>
-                                        <span class="nav-main-link-name ms-3">Résultat Étudian</span>
+                                        <span class="nav-main-link-name ms-3">Résultat Étudiant </span>
                                     </a>
                                 </li>
                                 <li class="nav-main-item">
                                     <a class="nav-main-link" href="{{ route('insertion.module.form') }}">
                                         <i class="fa fa-file-word fa-2x text-muted"></i>
-                                        <span class="nav-main-link-name ms-3">Résultat Étudians Par Module</span>
+                                        <span class="nav-main-link-name ms-3">Résultat Étudiants Par Module</span>
                                     </a>
                                 </li>
                                 <li class="nav-main-item">
                                     <a class="nav-main-link" href="{{ route('doctorat.inscription.show') }}">
                                         <i class="fa fa-file-word fa-2x text-muted"></i>
-                                        <span class="nav-main-link-name ms-3">Inscription Doctorat</span>
+                                        <span class="nav-main-link-name ms-3">Inscription Administrative Doctorat</span>
                                     </a>
                                 </li>
                                 <li class="nav-main-item">
@@ -153,6 +153,18 @@
                                     <a class="nav-main-link" href="{{ route('compte-fonctionnel-apogee.show') }}">
                                         <i class="fa fa-file-word fa-2x text-muted"></i>
                                         <span class="nav-main-link-name ms-3">Nouvelle Spécialité Doctorat</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a class="nav-main-link" href="{{ route('annulation.inscription.form') }}">
+                                        <i class="fa fa-file-word fa-2x text-muted"></i>
+                                        <span class="nav-main-link-name ms-3">Annulation d'Inscription</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a class="nav-main-link" href="{{ route('suppression.note.etudiant.form') }}">
+                                        <i class="fa fa-file-word fa-2x text-muted"></i>
+                                        <span class="nav-main-link-name ms-3">suppression des notes</span>
                                     </a>
                                 </li>
                             </ul>
